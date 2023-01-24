4e586980df9b2d61ac148508105c519cc25de769
felipeyukio182 <felipeyukio182@gmail.com>
2023-01-24 09:39:35 -0300

    feat: criado rota temporaria de login para testes

--- src/login/LoginController.ts
+++ src/login/LoginController.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { auth } from "./config/auth";
 
 export const LoginController = (req: Request, res: Response) => {
-  res.status(200).json({ login: "login", query: req.query });
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new Error("Credenciais inválidas!");
+    }
+
+    const jwt = sign({ email, password }, auth.jwt.secret, {
+      expiresIn: auth.jwt.expiresIn,
+    });
+
+    res.status(200).json({ email: email, token: jwt });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(401).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "Erro inesperado" });
+    }
+  }
 };
--- src/login/route.ts
+++ src/login/route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { LoginController } from "./LoginController";
 const router = Router();
 
-router.get("/login", LoginController);
+router.post("/login", LoginController);
 
 export default router;
